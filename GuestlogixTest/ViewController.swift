34e2d7641977732f28dd0b74364700a920f3eab6
Byung Yoo <Byung@Byungs-MacBook-Air.local>
2018-09-24 21:04:13 -0400

    Guestlogix Test

---
+++ GuestlogixTest/ViewController.swift
@@ -0,0 +1,26 @@
+blah //
+//  ViewController.swift
+//  GuestlogixTest
+//
+//  Created by Byung Yoo on 2018-09-24.
+//  Copyright © 2018 Theta Labs Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    // aa
+}
+
---
+++ Pods/Target Support Files/Pods-GuestlogixTest/Pods-GuestlogixTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GuestlogixTest : NSObject
+@end
+@implementation PodsDummy_Pods_GuestlogixTest
+@end
